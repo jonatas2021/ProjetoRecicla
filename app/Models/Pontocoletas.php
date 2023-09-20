4988d0eb878f110538ae424dd065c960c2d50a1b
jonatas2021 <jonatascandido0@gmail.com>
2023-09-20 15:33:20 -0300

    First

---
+++ app/Models/Pontocoletas.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Pontocoletas extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'status',
+        'latitude',
+        'longetudi',
+        'user_id'
+    
+    ];
+
+
+    public function empresa(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
