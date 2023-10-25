67fc660ee8f72107eef29ee41a73bba0dcffb7bf
Denils0n <doc@discente.ifpe.edu.br>
2023-10-25 19:16:13 -0300

    Criando e configurando migrate e model de Company

---
+++ app/Models/Company.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+
+        'name',
+        'cnpj',
+        'user_id',
+
+
+    ];
+
+    public function owner(): BelongsTo
+    {
+
+
+        return $this->belongsTo(User::class, 'user_id');
+
+
+    }
+
+
+}
---
+++ database/migrations/2023_10_25_220622_create_companies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('cnpj');
+            $table->foreignId('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
