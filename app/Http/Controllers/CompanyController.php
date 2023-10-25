eacd7a9f5d50801037214b6dc91ed35ca0eba3c8
Denils0n <doc@discente.ifpe.edu.br>
2023-10-25 19:40:06 -0300

    criando o index no Controller e a pasta index da Company

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -12,7 +12,12 @@ class CompanyController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Company/index', [
+
+            'company' => Auth::user()->company
+
+
+        ]);
     }
 
     /**
