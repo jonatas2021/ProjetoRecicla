646acffd714ddbae2d4edc0eb94697b62f9cddf7
Denils0n <doc@discente.ifpe.edu.br>
2023-10-25 19:28:17 -0300

    Criando as function do relacionamento do User e Company

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use \Illuminate\Database\Eloquent\Relations\HasOne;
+
 
 class User extends Authenticatable
 {
@@ -42,4 +44,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function company(): HasOne
+    {
+        return $this->hasOne(Company::class);
+    }
+
+
+
 }
