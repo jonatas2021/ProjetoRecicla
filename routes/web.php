78b9a798e20c105730b251eb24d086ddb95a77b0
Denils0n <doc@discente.ifpe.edu.br>
2023-10-25 19:49:28 -0300

    criando rotas para company

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controler\CompanyController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +35,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::redirect('/company', CompanyController::class);
+
+
 });
 
 require __DIR__.'/auth.php';
