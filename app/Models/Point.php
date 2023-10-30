109de0144a381c1d237715ccfe96ee9138672aff
SiOlimpia <soss@discente.ifpe.edu.br>
2023-10-30 17:57:35 -0300

    Criando model point

--- app/Models/Point.php
+++ app/Models/Point.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Point extends Model
-{ 
+{
     use HasFactory;
+
+    protected $fillable = [
+
+
+        'name',
+        'complement',
+        'longitude',
+        'latitude',
+        'company_id',
+
+    ];
+    
 }
