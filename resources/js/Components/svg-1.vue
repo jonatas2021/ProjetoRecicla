6e52454002b91e8d009a0f7f8ab4fbc920aa3021
Layrton07 <layrtoncavalcanti09@gmail.com>
2023-11-20 14:25:28 -0300

    Criacao do component do svg 1

---
+++ resources/js/Components/svg-1.vue
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<template>
+    <div class="box-1 sm:position:relative right-0">
+        <div class="grafic-color-yellow">
+            <div class="card-box w-96">
+                <div class="card-text">
+                    <h1>lorem ipsum</h1>
+                    <p class="break-words">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem adipisci molestias iure, doloremque natus corporis officiis vel itaque corrupti. Dignissimos quia porro autem laboriosam aperiam rem pariatur facere iusto repellendus!</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style>
+.grafic-color-yellow{
+    width: 100vw;
+    height: 100vh;
+    /* background-color: rgba(255, 0, 0, 0.4); */
+    background-image: url('../pictures/svg-1.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-position-x: 300px;
+}
+.card-box{
+    /*width: 500px;*/
+    background-color: rgba(255,0,0,0.5);
+}
+</style>
