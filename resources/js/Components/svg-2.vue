856f85a1a9818ae92b1ff03f751076476853b2e7
Layrton07 <layrtoncavalcanti09@gmail.com>
2023-11-20 14:28:18 -0300

    Criacao do component do svg 2

---
+++ resources/js/Components/svg-2.vue
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<template>
+    <div class="box-2">
+        <div class="grafic-color-blue"></div>
+    </div>
+</template>
+
+<style>
+.box-2{
+    position: relative;
+    top: -150px;
+}
+.grafic-color-blue{
+    width: 100vw;
+    height: 100vh;
+    /* background-color: rgba(0, 255, 255, 0.4) */
+    background-image: url('../pictures/svg-2.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-position-x: -340px;
+    }
+</style>
