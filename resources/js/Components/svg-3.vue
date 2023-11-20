cf02623a0bd07e2fc84f130697b957122133d115
Layrton07 <layrtoncavalcanti09@gmail.com>
2023-11-20 14:30:20 -0300

    Criacao do component do svg 3

---
+++ resources/js/Components/svg-3.vue
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<template>
+    <div class="box-3">
+        <div class="grafic-color-green"></div>
+    </div>
+</template>
+
+<style>
+.box-3{
+    position: relative;
+    top: -300px;
+}
+.grafic-color-green{
+    width: 100vw;
+    height: 100vh;
+    /* background-color: rgba(0, 255, 255, 0.4) */
+    background-image: url('../pictures/svg-3.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-position-x: 400px;
+    }
+</style>
