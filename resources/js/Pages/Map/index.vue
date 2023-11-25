3ac21454105955425e7c1b9b64781d4465124303
Jack <doc@discente.ifpe.edu.br>
2023-11-25 02:30:30 +0200

    Criando pasta e arquivo index do map

---
+++ resources/js/Pages/Map/index.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+    import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+    import { Head, useForm } from "@inertiajs/vue3";
+    
+
+    defineProps({
+
+        Points:{
+
+            type:Object,
+            required: true
+                
+
+
+
+        }
+    }) 
+
+
+</script>
+
+<template>
+
+    <div v-for="Point in Points">
+
+
+        {{ Point }}
+
+
+
+    </div>
+
+
+
+</template>
