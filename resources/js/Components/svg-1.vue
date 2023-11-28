d425aad23e1c4483f1eb8f372d3b4e5037b1427c
Layrton07 <layrtoncavalcanti09@gmail.com>
2023-11-27 22:14:17 -0300

    adicao de layout grid e criacao de novo container

--- resources/js/Components/svg-1.vue
+++ resources/js/Components/svg-1.vue
@@ -4,10 +4,13 @@
 <template>
     <div class="box-1 sm:position:relative right-0">
         <div class="grafic-color-yellow">
-            <div class="card-box w-96">
+            <div class="card-box w-full grid grid-cols-2">
                 <div class="card-text">
                     <h1>lorem ipsum</h1>
                     <p class="break-words">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem adipisci molestias iure, doloremque natus corporis officiis vel itaque corrupti. Dignissimos quia porro autem laboriosam aperiam rem pariatur facere iusto repellendus!</p>
+                </div>
+                <div class="card-image">
+
                 </div>
             </div>
         </div>
@@ -19,14 +22,17 @@
 .grafic-color-yellow{
     width: 100vw;
     height: 100vh;
-    /* background-color: rgba(255, 0, 0, 0.4); */
     background-image: url('../pictures/svg-1.svg');
     background-size: 100% 100%;
     background-repeat: no-repeat;
     background-position-x: 300px;
 }
-.card-box{
-    /*width: 500px;*/
-    background-color: rgba(255,0,0,0.5);
+.card-text{
+    width: 50vw;
+    height: 100vh;
+}
+.card-image{
+    width: 50vw;
+    height: 100vh;
 }
 </style>
