19736598c4f376f1d9c322f6aac1e9ebd7f41d4e
Layrton07 <layrtoncavalcanti09@gmail.com>
2023-11-29 23:22:12 -0300

    adicao dos elementos visuais e estilizacao completo component: svg-3

--- resources/js/Components/svg-3.vue
+++ resources/js/Components/svg-3.vue
@@ -3,7 +3,20 @@
 
 <template>
     <div class="box-3">
-        <div class="grafic-color-green"></div>
+        <div class="grafic-color-green">
+            <div class="block-cont-2 grid grid-cols-2 w-full">
+                <div class="content-image flex justify-center items-center">
+                    <div class="image-svg-2 w-3/4 h-3/4"></div>
+                </div>
+                <div class="content-text flex justify-center items-center">
+                    <div class="box-content w-3/4 break-all">
+                        <h3 class="text-4xl mb-8 font-medium">Sustentabilidade</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,6 +24,11 @@
 .box-3{
     position: relative;
     top: -300px;
+}
+.image-svg-2{
+    background-image: url('../pictures/Save-the-Earth-cuate.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
 }
 .grafic-color-green{
     width: 100vw;
