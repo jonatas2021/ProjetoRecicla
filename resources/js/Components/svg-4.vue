ace74a31be8d7fb61e76bb1dbed4542e1d3c098b
Layrton07 <layrtoncavalcanti09@gmail.com>
2023-11-29 23:50:48 -0300

    Criacao do component svg-4

---
+++ resources/js/Components/svg-4.vue
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<template>
+    <div class="box-4">
+        <div class="grafic-color-red"></div>
+    </div>
+</template>
+
+<style>
+.box-4{
+    position: relative;
+    top: -450px;
+}
+.grafic-color-red{
+    width: 100vw;
+    height: 100vh;
+    /* background-color: rgba(0, 255, 255, 0.4) */
+    background-image: url('../pictures/svg-4.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-position-x: -240px;
+}
+</style>
