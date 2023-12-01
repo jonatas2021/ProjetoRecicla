08b4b3564529eb23f821b331599a6ba4525d9b23
Layrton07 <layrtoncavalcanti09@gmail.com>
2023-11-30 23:16:37 -0300

    Criacao do component Equipe

---
+++ resources/js/Components/Team.vue
@@ -0,0 +1,39 @@
+<script>
+</script>
+
+<template>
+    <div class="box-5">
+        <div class="team-box">
+            <div class="block-cont-4 grid grid-cols-2 w-full">
+                <div class="content-image-team flex justify-center items-center">
+                    <div class="image-svg-4 w-3/4 h-3/4"></div>
+                </div>
+                <div class="content-text flex justify-center items-center">
+                    <div class="box-content w-2/4 break-all">
+                        <h3 class="text-4xl mb-8 font-medium">Equipe</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .box-5{
+    position: relative;
+    top: -830px;
+    height: 0vh;
+    margin-bottom: 0;
+}
+.content-image-team{
+    width: 50vw;
+    height: 70vh;
+}
+.image-svg-4{
+    background-image: url('../pictures/teamwork-high-five-cuate.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+}
+</style>
