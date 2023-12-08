6b2ad328d634688b57bc14783c63ccd393b68712
Layrton07 <layrtoncavalcanti09@gmail.com>
2023-12-08 01:19:59 -0300

    adicao dos icones e adicao de ancora aos icones

--- resources/js/Components/footer.vue
+++ resources/js/Components/footer.vue
@@ -14,18 +14,18 @@
                 </div>
                 <div class="card-foot absolute w-full h-60">
                     <div class="text-footer absolute ml-24 flex gap-6 mt-5 z-20">
-                        <a href="">home</a>
-                        <a href="">About</a>
-                        <a href="">Contact</a>
+                        <a href="" class="dark:hover:text-gray-600 transition duration-1000 rounded-lg p-2 hover:bg-amber focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+                        <a href="" class="dark:hover:text-gray-600 transition duration-1000 rounded-lg p-2 hover:bg-amber focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">About</a>
+                        <a href="" class="dark:hover:text-gray-600 transition duration-1000 rounded-lg p-2 hover:bg-amber focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Contact</a>
                     </div>
                     <div class="network z-20 flex justify-center items-center h-full">
                         <hr class="z-20 w-standard decoration-black mt-20 absolute">
                         <div class="network-icons flex gap-6 justify-end items-end h-full w-full mr-24 mb-10">
-                            <div class="facebook bg-red-500 w-10 h-10 "></div>
-                            <div class="linkedin bg-blue-500 w-10 h-10"></div>
-                            <div class="twitter bg-green-500 w-10 h-10"></div>
-                            <div class="youtube bg-yellow-500 w-10 h-10"></div>
-                            <div class="instagram bg-orange-500 w-10 h-10"></div>
+                            <a href="" class="facebook w-10 h-10 "></a>
+                            <a href="" class="linkedin w-10 h-10"></a>
+                            <a href="" class="twitter w-10 h-10"></a>
+                            <a href="" class="youtube w-10 h-10"></a>
+                            <a href="" class="instagram w-10 h-10"></a>
                         </div>
                     </div>
                 </div>
@@ -68,6 +68,28 @@ hr{
     border-top-width: 2px;
 }
 .facebook{
-
+    background-image: url('../pictures/facebook-icon.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+}
+.linkedin{
+    background-image: url('../pictures/linkedin-icon.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+}
+.twitter{
+    background-image: url('../pictures/twitter-icon.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+}
+.youtube{
+    background-image: url('../pictures/youtube-icon.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+}
+.instagram{
+    background-image: url('../pictures/Instagram-icon.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
 }
 </style>
