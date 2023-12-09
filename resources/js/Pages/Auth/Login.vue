df35d1855eeb9601247aee9b8efcba02f56a80e9
Layrton07 <layrtoncavalcanti09@gmail.com>
2023-12-08 21:45:08 -0300

    adicao do estilo da tela de login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Checkbox from '@/Components/Checkbox.vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
+// import GuestLayout from '@/Layouts/GuestLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
@@ -30,7 +30,93 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
+    <Head title="Log in" />
+    <div class="nav w-screen absolute">
+        <div class="flex items-center justify-start font-semibold gap-6 ml-28">
+            <a href=""
+                class="dark:hover:text-gray-600 transition duration-1000 rounded-lg p-2 hover:bg-amber focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+            <a href=""
+                class="dark:hover:text-gray-600 transition duration-1000 rounded-lg p-2 hover:bg-amber focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">About</a>
+            <a href="/"
+                class="dark:hover:text-gray-600 transition duration-1000 rounded-lg p-2 hover:bg-amber focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Voltar</a>
+        </div>
+    </div>
+
+    <div class="box-1 sm:position:relative right-0">
+        <div class="grafic-color-yellow">
+            <div class="card-box w-full grid grid-cols-2">
+                <div class="card-text flex items-center justify-center">
+                    <div class="text-box w-3/4 break-all">
+                        <h2 class="text-6xl font-extrabold mb-8">login</h2>
+                        <p class="break-words">Insira os detalhes da sua conta</p>
+                        <div class="box-btn flex w-3/4 mt-16 gap-6">
+
+                            <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                                {{ status }}
+                            </div>
+
+                            <form @submit.prevent="submit" class="w-4/5">
+                                <div>
+                                    <InputLabel for="email" value="Email" />
+
+                                    <TextInput id="email" type="email" class="mt-1 block w-full outline-none"
+                                        v-model="form.email" required autofocus autocomplete="username" />
+
+                                    <InputError class="mt-2" :message="form.errors.email" />
+                                </div>
+
+                                <div class="mt-4">
+                                    <InputLabel for="password" value="Password" />
+
+                                    <TextInput id="password" type="password" class="mt-1 block w-full"
+                                        v-model="form.password" required autocomplete="current-password" />
+
+                                    <InputError class="mt-2" :message="form.errors.password" />
+                                </div>
+
+                                <div class="block mt-4 grid grid-cols-2 w-full">
+                                    <label class="flex items-center">
+                                        <Checkbox name="remember" v-model:checked="form.remember" />
+                                        <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                                    </label>
+                                    <Link v-if="canResetPassword" :href="route('password.request')"
+                                        class="underline text-sm text-gray-600 hover:text-gray-900 flex justify-end rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    Esqueceu sua senha?
+                                    </Link>
+                                </div>
+
+                                <PrimaryButton class="flex items-center justify-center w-full h-10 mt-16" :class="{ 'opacity-25': form.processing }"
+                                        :disabled="form.processing">
+                                        Log in
+                                    </PrimaryButton>
+                                
+                                <div class="flex items-center mt-16">
+                                    <Link v-if="canResetPassword" :href="route('register')"
+                                        class="underline w-full text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    Não tem uma conta?
+                                    </Link>
+                                    <div class="relative w-full flex justify-end items-center ">
+                                        <a href="register" class="border border-solid border-gray-800 hover:bg-gray-700 ml-4 p-2 text-center bg-gray-800 text-white h-9 flex items-center justify-center transition duration-1000 rounded-lg focus:outline-red-500">Inscrever-se</a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-image relative">
+                    <div class="card-image-text">
+                        <h1 class="text-7xl font-extrabold text-amber mb-3 indent-32">Reciclaí</h1>
+                        <p class="text-amber indent-32">Login to access your account</p>
+                    </div>
+                    <div class="waste-management absolute bottom-0 right-0">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- <GuestLayout>
         <Head title="Log in" />
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
@@ -90,5 +176,39 @@ const submit = () => {
                 </PrimaryButton>
             </div>
         </form>
-    </GuestLayout>
+    </GuestLayout> -->
 </template>
+<style>
+.grafic-color-yellow {
+    width: 100vw;
+    height: 100vh;
+    background-image: url('../../pictures/svg-1.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-position-x: 300px;
+}
+
+.card-text {
+    width: 50vw;
+    height: 100vh;
+}
+
+.card-image {
+    width: 50vw;
+    height: 100vh;
+}
+
+.card-image-text {
+    position: absolute;
+    top: 22%;
+}
+
+.waste-management {
+    /* background-color: red; */
+    width: 90%;
+    height: 60%;
+    background-image: url('../../pictures/Waste management-cuate.svg');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+}
+</style>
