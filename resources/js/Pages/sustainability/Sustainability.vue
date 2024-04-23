51e8b1a24620e5a40c0a3c10facaf935c7735519
Layrton07 <layrtoncavalcanti09@gmail.com>
2024-04-23 11:14:47 -0300

    criacao da tela de sustentabilidade

---
+++ resources/js/Pages/sustainability/Sustainability.vue
@@ -0,0 +1,29 @@
+<script>
+import {Head} from '@inertiajs/vue3'
+import Sustainability_1 from "../../Components/Sustainability-svg-1.vue"
+import Sustainability_2 from "../../Components/Sustainability-svg-2.vue"
+import Sustainability_3 from "../../Components/Sustainability-svg-3.vue"
+import Sustainability_4 from "../../Components/Sustainability-svg-4.vue"
+import Sustainability_5 from "../../Components/Sustainability-svg-5.vue"
+export default{
+    components:{
+        Head,
+        Sustainability_1,
+        Sustainability_2,
+        Sustainability_3,
+        Sustainability_4,
+        Sustainability_5,
+
+    }
+}
+</script>
+<template>
+    <Head title="Sustentabilidade"/>
+    <Sustainability_1></Sustainability_1>
+    <Sustainability_2></Sustainability_2>
+    <Sustainability_3></Sustainability_3>
+    <Sustainability_4></Sustainability_4>
+    <Sustainability_5></Sustainability_5>
+</template>
+<style>
+</style>
