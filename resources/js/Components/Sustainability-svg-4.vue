adaa9a30d7400633f063abf8cb777867f381d387
Layrton07 <layrtoncavalcanti09@gmail.com>
2024-04-23 11:10:29 -0300

    criacao do quarto componente da tela de sustentabilidade

---
+++ resources/js/Components/Sustainability-svg-4.vue
@@ -0,0 +1,25 @@
+<script>
+</script>
+<template>
+    <div class="w-screen h-screen bg-newGreen">
+        <div class="Sustainability_3 w-full h-full">
+            <div class="w-2/4 h-full flex justify-end">
+                <div class="w-3/5 h-full flex flex-col justify-center">
+                    <h1 class="text-3xl mb-8 text-textNewBlue font-medium">Lorem</h1>
+                    <p class="text-2xl text-white font-medium">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 
+mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+    .Sustainability_3{
+        background-image: url('../pictures/Sustainability-svg-3.svg');
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        background-position-x: -240px;
+        position: relative;
+        top: -400px;
+    }
+</style>
