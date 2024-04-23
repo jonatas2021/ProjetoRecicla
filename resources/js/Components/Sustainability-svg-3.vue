1f674278e494585b9f04223cc2755caf50f0f031
Layrton07 <layrtoncavalcanti09@gmail.com>
2024-04-23 11:10:00 -0300

    criacao do terceiro componente da tela de sustentabilidade

---
+++ resources/js/Components/Sustainability-svg-3.vue
@@ -0,0 +1,25 @@
+<script>
+</script>
+<template>
+    <div class="w-screen h-screen bg-newGreen">
+        <div class="Sustainability_2 w-full h-full flex justify-end">
+            <div class="w-2/4 h-full">
+                <div class="w-3/5 h-full flex flex-col justify-center">
+                    <h2 class="text-3xl mb-8 text-textNewBlue font-medium">De que forma esses materiais podem afetar a nossa saúde ?De que forma esses materiais podem afetar a nossa saúde ?</h2>
+                    <p class="text-2xl text-white font-medium">Os materiais contidos nas pilhas e baterias trazem diversos males a nossa saúde. Caso você tenha contato direto com o mercúrio, ele pode afetar o nosso pulmão, o sistema neurológio e até causar insuficiência renal. Eles ainda podem contaminar o solo, rios e lençóis freáticos, podendo estar presentes na água ou alimentos que consumimos.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+<style>
+    .Sustainability_2{
+        background-image: url('../pictures/Sustainability-svg-2.svg');
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        background-position-x: 400px;
+        position: relative;
+        top: -280px;
+    }
+</style>
