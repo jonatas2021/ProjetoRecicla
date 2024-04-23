b7e60848c96ea1652d06fd6232e4ed1fb72357b5
Layrton07 <layrtoncavalcanti09@gmail.com>
2024-04-23 11:08:21 -0300

    criacao do primeiro componente da tela de sustentabilidade

---
+++ resources/js/Components/Sustainability-svg-1.vue
@@ -0,0 +1,15 @@
+<script>
+</script>
+<template>
+    <div class="w-screen h-screen bg-newGreen flex flex-col items-center">
+        <h1 class="text-5xl font-medium mt-28 text-textNewBlue">Sustentabilidade</h1>
+        <div class="earth w-4/5 h-4/5 mt-20 z-10"></div>
+    </div>
+</template>
+<style>
+.earth{
+    background-image: url('../pictures/Save-the-Earth-cuate.svg');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+}
+</style>
